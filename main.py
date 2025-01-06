55cd3454265ce4e425f64da720dca9969ee39acf
bebopkenny <kennygarcia15@csu.fullerton.edu>
2025-01-05 22:26:48 -0800

    Created the blocks and instructions

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ class DrawInformation:
     WHITE = 255, 255, 255
     BLUE = 70,130,180
     RED = 255, 0, 0
-    GREY = 128, 128, 128
     BACKGROUND_COLOR = WHITE
+
+    GRADIENTS = [
+        (128, 128, 128),
+        (160, 160, 160),
+        (192, 192, 192)
+    ]
+
+    FONT = pygame.font.SysFont('comicsans', 20)
+    LARGE_FONT = pygame.font.SysFont('comicsans', 10)
     
     SIDE_PAD = 100
     TOP_PAD = 150
@@ -23,13 +31,37 @@ class DrawInformation:
         
     def set_list(self, lst):
         self.lst = lst
-        self.min_val = max(lst)        
+        self.min_val = min(lst)        
         self.max_val = max(lst)
+
 
         self.block_width = round((self.width - self.SIDE_PAD) / len(lst)) # width of the block
         self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val)) # height of the block
         self.start_x = self.SIDE_PAD // 2
         
+def draw(draw_info):
+    draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+    
+    controls = draw_info.FONT.render("R - Reset | SPACE - Start Sorting | A - Ascending | D - Descending", 1, draw_info.BLACK)
+    draw_info.window.blit(controls, (draw_info.width / 2 - controls.get_width() / 2, 5))
+
+    sorting = draw_info.FONT.render("I - Insertion Sort | B - Bubble Sort", 1, draw_info.BLACK)
+    draw_info.window.blit(sorting, (draw_info.width / 2 - sorting.get_width() / 2, 35))
+
+    draw_list(draw_info)
+    pygame.display.update()
+
+def draw_list(draw_info):
+    lst = draw_info.lst
+
+    for i, val in enumerate(lst):
+        x = draw_info.start_x + i * draw_info.block_width
+        y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
+
+        color = draw_info.GRADIENTS[i % 3] 
+
+        pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.height))
+
 def generate_starting_list(n, min_val, max_val):
     lst = []
     for _ in range(n):
@@ -40,23 +72,41 @@ def generate_starting_list(n, min_val, max_val):
     
 def main():
     clock = pygame.time.Clock()
-    run = True
+
     
     n = 50
     min_val = 0
     max_val = 100
     
-    lst = generate_starting_list(n, min_val, max)
+    lst = generate_starting_list(n, min_val, max_val)
     draw_info = DrawInformation(800, 600, lst)
+    run = True
+    sorting = False
+    ascending = True
     
     while run:
          clock.tick(60) # FPS
+         draw(draw_info)
          
          pygame.display.update()
          
          for event in pygame.event.get():
              if event.type == pygame.QUIT:
                  run = False
+             if event.type != pygame.KEYDOWN:
+                 continue
+             if event.key == pygame.K_r:
+                lst = generate_starting_list(n, min_val, max_val)
+                draw_info.set_list(lst)
+                sorting = False
+             elif event.key == pygame.K_SPACE and sorting == False:
+                sorting = True
+             elif event.key == pygame.K_a and not sorting:
+                ascending = True
+             elif event.key == pygame.K_d and not sorting:
+                 ascending = False
+
+
     pygame.quit()
     
 if __name__ == "__main__":
